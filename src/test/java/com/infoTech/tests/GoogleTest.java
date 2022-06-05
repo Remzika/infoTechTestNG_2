646e316687bf05e43006a5e9c27acfbd8d547214
remzi <remzika58@gmail.com>
2022-06-05 16:35:53 +0200

    ilk version

---
+++ src/test/java/com/infoTech/tests/GoogleTest.java
@@ -0,0 +1,16 @@
+package com.infoTech.tests;
+
+import com.infoTech.pages.GooglePage;
+import com.infoTech.utilities.TestBase;
+import org.testng.annotations.Test;
+public class GoogleTest extends TestBase {
+
+    @Test
+    public void aramaTesti() throws InterruptedException {
+        driver.get("https://google.com");
+        GooglePage page=new GooglePage(driver);
+        Thread.sleep(3000);
+        page.guvenlikDuvari.click();
+        page.aramaKutusu.sendKeys("page'e selam olsun");
+    }
+}
---
+++ src/test/java/com/infoTech/tests/fileDowland.java
@@ -0,0 +1,16 @@
+package com.infoTech.tests;
+
+import com.infoTech.utilities.TestBase;
+import org.testng.annotations.Test;
+
+public class fileDowland extends TestBase {
+
+    @Test
+    public void dosyaVarmiYokmu(){
+        System.out.println(System.getProperty("user.dir"));
+
+        System.out.println(System.getProperty("user.home"));
+
+
+    }
+}
